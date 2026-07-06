644ce5e9108c56752117f27ea10435a48a5d2b87
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2026-07-06 21:23:29 +0200

    Create items.js

---
+++ js/components/items.js
@@ -0,0 +1,26 @@
+const list = [
+ {
+  name: "cotton socks",
+  price: "5.99",
+  discprice: "4.99",
+  srcimg: "img/cotton-socks.png"
+ },
+ {
+  name: "tennis balls",
+  price: "20.40",
+  discprice: "15.99",
+  srcimg: "img/tennis-balls.png"
+ },
+ {
+  name: "plain t shirt",
+  price: "120.30",
+  discprice: "100.82",
+  srcimg: "img/plain-t-shirt.png"
+ },
+ {
+  name: "itel phone",
+  price: "500.99",
+  discprice: "450.99",
+  srcimg: "img/phone.png"
+ }
+];
